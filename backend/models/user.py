186e41e3f012e5967b16c85e2a09adf5e620b9c2
Jose Developer <jeduramos7001@gmail.com>
2026-03-20 02:04:45 -0600

    Corregir propiedades de atributos en modelos Role y User

--- backend/models/user.py
+++ backend/models/user.py
@@ -10,10 +10,14 @@ class User(Base):
     id = Column(String(15), primary_key=True, index=True)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
     role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
-    is_active = Column(Boolean, nullable=False)
     
-    # Opcional
+    # Opcional, pero:
+    # Relacion con el modelo de rol para SQLAlchemy
     role = relationship("Role", back_populates="users")
+    
+    # Recomendado para relaciones inversas entre tablas
+    #user = relationship("User", back_populates="roles")
