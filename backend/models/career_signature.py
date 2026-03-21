d4e0b16e869e5b5ffb446101c4a745bfd3f07d9a
Jose Developer <jeduramos7001@gmail.com>
2026-03-20 23:39:04 -0600

    Corregir:Renombrar modelo SignatureCareer a CareerSignature

--- backend/models/career_signature.py
+++ backend/models/career_signature.py
@@ -2,14 +2,14 @@ from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from config.connection import Base
 
-# Modelo de materia para la base de datos
-class SignatureCareer(Base):
+# Modelo de carrera-asignatura para la base de datos
+class CareerSignature(Base):
     # Nombre de la tabla en la base de datos
-    __tablename__ = "signature_career"
+    __tablename__ = "career_signatures"
     
     id = Column(String(15), primary_key=True)
     signature_id = Column(String(15), ForeignKey("signatures.id"))
     career_id = Column(String(15), ForeignKey("careers.id"))
     
     # Relacion inversa para SQLAlchemy
-    group = relationship("Group", back_populates="signature_careers")
+    group = relationship("Group", back_populates="career_signatures")
